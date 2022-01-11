97de3b353871b6d4769f5b5fc0605be32e90f522
Abhinav Tyagi <abhinav_t@pp.iitr.ac.in>
2022-01-11 12:43:48 +0530

    Add Project Serializer

--- backend/bugphile_app/api/serializers/projectSerializer.py
+++ backend/bugphile_app/api/serializers/projectSerializer.py
@@ -1,9 +1,21 @@
-from django.core import serializers
-from django.core.serializers import serialize
 from rest_framework import serializers
-from models.project import Project, ProjectStatus
+from models.project import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    members = serializers.SerializerMethodField()
+
+    def get_members(self, obj):
+        members_list = []
+        for user in list(obj.members.all()):
+            user_data = {
+                'id': user.id,
+                'name': user.name,
+                'display_picture': user.display_picture
+            }
+            members_list.append(user_data)
+        return members_list
+
+
     class Meta:
         model = Project
         fields = '__all__'
